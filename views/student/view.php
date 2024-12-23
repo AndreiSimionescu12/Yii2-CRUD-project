0e9a46613d4f89a07ea520424713b8ea896b1ea6
AndreiSimionescu12 <andreisimionescu2000@gmail.com>
2024-12-23 21:54:00 +0200

    implement css view page

--- views/student/view.php
+++ views/student/view.php
@@ -1,42 +1,58 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\DetailView;
 
 /** @var yii\web\View $this */
 /** @var app\models\Student $model */
 
-$this->title = $model->id;
+$this->title = $model->last_name . ' ' . $model->first_name;
 $this->params['breadcrumbs'][] = ['label' => 'Students', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="student-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'last_name',
-            'first_name',
-            'faculty',
-            'specialization',
-            'group',
-            'year',
-        ],
-    ]) ?>
+    <div class="card shadow-sm">
+        <div class="card-header bg-dark text-white">
+            <h5 class="mb-0">Detalii Student: <?= Html::encode($this->title) ?></h5>
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+                    <th>Nume</th>
+                    <th>Prenume</th>
+                    <th>Facultate</th>
+                    <th>Specializare</th>
+                    <th>Grupă</th>
+                    <th>An de studiu</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td><?= Html::encode($model->last_name) ?></td>
+                    <td><?= Html::encode($model->first_name) ?></td>
+                    <td><?= Html::encode($model->faculty) ?></td>
+                    <td><?= Html::encode($model->specialization) ?></td>
+                    <td><?= Html::encode($model->group) ?></td>
+                    <td><?= Html::encode($model->year) ?></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="card-footer d-flex justify-content-between">
+            <?= Html::a('Înapoi', ['index'], ['class' => 'btn btn-secondary']) ?>
+            <div>
+                <?= Html::a('Modifică', ['update', 'id' => $model->id], ['class' => 'btn btn-primary me-2']) ?>
+                <?= Html::a('Șterge', ['delete', 'id' => $model->id], [
+                    'class' => 'btn btn-danger',
+                    'data' => [
+                        'confirm' => 'Ești sigur că vrei să ștergi acest student?',
+                        'method' => 'post',
+                    ],
+                ]) ?>
+            </div>
+        </div>
+    </div>
 
 </div>
